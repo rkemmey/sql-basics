00e3755cca61483f54a51f5e0e8f207bdb284303
rkemmey <rachel.kemmey@gmail.com>
2025-02-27 14:09:29 -0600

    updates

--- create_schema.sql
+++ create_schema.sql
@@ -1,5 +1,9 @@
 -- Schema
 DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS addresses;
+DROP TABLE IF EXISTS classes;
+DROP TABLE IF EXISTS enrollments;
+
 CREATE TABLE students (
   id           serial PRIMARY KEY,
   first_name   varchar(255) NOT NULL,
@@ -7,3 +11,28 @@ CREATE TABLE students (
   birthdate    date NOT NULL,
   address_id   integer
 );
+
+CREATE TABLE addresses (
+  id serial PRIMARY KEY,
+  line_1 varchar(255), 
+  city varchar(255),
+  state varchar(255),
+  zipcode varchar(255)
+
+);
+
+CREATE TABLE classes (
+  id serial PRIMARY KEY,
+  name varchar(255),
+  credits integer
+
+);
+
+CREATE TABLE enrollments (
+  id serial PRIMARY KEY,
+  student_id integer,
+  class_id integer,
+  grade varchar(255)
+
+);
+
